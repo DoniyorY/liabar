223bf7241a929aa1503b20a2fa0300d2479915c4
V1CTUS <yusupovdoniyor54@gmail.com>
2024-07-26 10:20:27 +0500

    backend

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -70,7 +70,10 @@ class MenuController extends Controller
         $model = new Menu();
 
         if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
+            if ($model->load($this->request->post())) {
+                $model->created=time();
+                $model->status=0;
+                $model->save();
                 return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
